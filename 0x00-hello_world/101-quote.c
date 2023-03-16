119bdbc41eb0ee810691c83b72d96fef0ab9efba
fletcherayoub <douch202@gmail.com>
2023-03-16 07:37:53 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
  * main - prints exatly "and that piece of art is useful" - Dora Korpar, 2015-10-19,
  * followed by new line, to the standard error.
- * Return: 0
+ * Return: 1
  */
 
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return(1)
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return(1);
 }
