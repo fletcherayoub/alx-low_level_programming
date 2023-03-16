4be7aecde47b4b4a8bf6157cc913b5e82b92134c
fletcherayoub <douch202@gmail.com>
2023-03-16 07:33:09 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - prints exatly "and that piece of art is useful"
- * - Dora Korpar, 2015-10-19
+ * main - prints exatly "and that piece of art is useful" - Dora Korpar, 2015-10-19,
+ * followed by new line, to the standard error.
  * Return: 0
  */
+
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
