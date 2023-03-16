0627c30bce807c50ed62cfda282296e224296506
fletcherayoub <douch202@gmail.com>
2023-03-16 04:28:39 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *  main - program print line with puts function
+ *  Resturn : 0 (Success)
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
