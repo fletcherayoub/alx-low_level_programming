1c619c44b9332601b4922ffdbfe2c7400b79ead7
fletcherayoub <douch202@gmail.com>
2023-04-03 08:27:34 -0700

    ayoub

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,12 +6,16 @@
  * @n: index
  * return: the memory area *s
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i;
-for (i = 0; i < n; i++)
-{
-s[i] = b;
-}
-return (s);
+
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
 }
