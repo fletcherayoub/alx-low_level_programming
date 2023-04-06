86c587636cac078ee11bf49dac131a54e2de3df1
fletcherayoub <douch202@gmail.com>
2023-04-06 05:40:57 -0700

    today with myself

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print the name, followed by a new line
+ * @argc: count argument
+ * @argv: Arguments
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+today argc,argv
