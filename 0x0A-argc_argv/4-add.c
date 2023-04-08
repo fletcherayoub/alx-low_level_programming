d91e1cafb189006c6347ef699a6e659b7d708831
fletcherayoub <douch202@gmail.com>
2023-04-07 17:52:01 -0700

    me

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -5,21 +5,39 @@
 #include "main.h"
 
 /**
- * main - program that adds positive numbers
+ * main - program that adds and returns the sum
  * @argc: number of command
  * @argv: array
  *
- * Return: 1 if not integer , 0 otherwise
+ * Return: 1 if not integer , 0 otherwis
  */
 
 int main(int argc, char *argv[])
 {
-	int n, m, len, sum;
+	int n, m, length, sum;
 	char *ptr;
 
-	if (arg < 2)
+	if (argc < 2)
 		printf("0\n");
 	else
 	{
 		sum = 0;
-		for (n = 1; n < argc; n++
+		for (n = 1; n < argc; n++)
+		{
+			ptr = argv[n];
+			length = strlen(ptr);
+
+			for (m = 0; m < length; m++)
+			{
+				if (isdigit(*(ptr + m)) == 0)
+				{
+					printf("error\n");
+					return (1);
+				}
+			}
+			sum += atoi(argv[n]);
+		}
+	printf("%d\n", sum);
+	}
+	return (0);
+}
