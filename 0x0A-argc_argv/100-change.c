218efbed4e78779726a16d9396c7bb000a4b215a
fletcherayoub <douch202@gmail.com>
2023-04-10 06:31:12 -0700

    argc argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,70 +1,48 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <stdbool.h>
 
 /**
- * converter - function that does all the mathematics
- * @n: variable from main
- * Return: the number of coins needed
- */
-
-int converter(int n)
-{
-	int count = 0;
-
-	while (n != 0)
-	{
-		if (n % 10 == 9 || n % 10 == 7)
-			n -= 2;
-		else if (n % 25 == 0)
-			n -= 25;
-		else if (n % 10 == 0)
-			n -= 10;
-		else if (n % 5 == 0)
-			n -= 5;
-		else if (n % 2 == 0)
-		{
-			if (n % 10 == 6)
-				n -= 1;
-			else
-				n -= 2;
-		}
-		else
-			n -= 1;
-		count++;
-	}
-	return (count);
-}
-
-/**
- * main - coin count
- * @argc: command line
- * @argv: array
+ * converter - change coins for an amount money
+ * @argc: number of arg
+ * @argv: array with arg
  *
- * Return: 0 if exactly 1 argument is passed
+ * Return: 0
  */
 
-int main(int argc, char *argv[])
+int converter(int argc, char *argv[])
 {
-	int n, coin;
-
-	coin = 0;
+	int n = 0, m = 0;
+	char c[] = "Error";
 
 	if (argc != 2)
 	{
-		printf("error\n");
+		printf("%s\n", c);
 		return (1);
 	}
 	n = atoi(argv[1]);
-
-	if (n < 0)
-		printf("0\n");
-	else
+	while (n >= 25)
 	{
-		coin = converter(n);
-		printf("%d\n", coin);
+		n -= 25;
+		m++;
 	}
+	while (n >= 10)
+	{
+		n -= 10;
+		m++;
+	}
+	while (n >= 5)
+	{
+		n -= 5;
+		m++;
+	}
+	while (n >= 2)
+	{
+		n -= 2;
+		m++;
+	}
+	if (n == 1)
+		m++;
+	printf("%d\n", m);
 	return (0);
 }
