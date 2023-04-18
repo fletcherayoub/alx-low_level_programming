b800897bd1f1495be7e453f5f9e2f5393ad74ae4
fletcherayoub <douch202@gmail.com>
2023-04-18 04:50:11 -0700

    hard things

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,12 +1,6 @@
 #ifndef _SUM_
 #define _SUM_
 
-/**
- * sum : 4-sum.h
- * macro function-like macro SUM(x, y) that computes the sum of the numbers
- *
- */
-
 #define SUM(x, y) ((x) + (y))
 
 #endif
