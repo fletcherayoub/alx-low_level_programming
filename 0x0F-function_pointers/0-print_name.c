305ae8ce949f3bc2da5d2d9b4caab5afb92a61b6
fletcherayoub <douch202@gmail.com>
2023-04-19 05:50:45 -0700

    we need time please

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: name to print
+ * @f: pointer
+ *
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+	{
+		f(name);
+	}
+}
