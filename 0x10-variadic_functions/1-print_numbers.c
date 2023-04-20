9f9f2784af20190b91816b7cf339de477ad4999d
fletcherayoub <douch202@gmail.com>
2023-04-20 06:39:05 -0700

    me again

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(args, int));
 
-		if (u != n - 1)
+		if (u < n - 1 && separator != NULL)
 			printf("%s", separator);
 	}
 	va_end(args);
