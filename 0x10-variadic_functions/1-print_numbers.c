0bd15e88d5fb7acd9e754148f6dfbecd944c3ad9
fletcherayoub <douch202@gmail.com>
2023-04-20 06:32:29 -0700

    i need more time

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * print_nambers - prints numbers followed by a new line
+ * @separator: printe between numbers
+ * @n: number of parameter
+ *
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int u;
+	va_list args;
+
+	va_start(args, n);
+
+	for (u = 0; u < n ; u++)
+	{
+		printf("%d", va_arg(args, int));
+
+		if (u != n -1)
+			printf("%s", separator);
+	}
+	va_end(args);
+	printf("\n");
+}
