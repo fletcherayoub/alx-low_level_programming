86d6703a4fca61f48d4958ad20852c3e154a9497
fletcherayoub <douch202@gmail.com>
2023-05-01 04:54:58 -0700

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,18 +6,22 @@
 /**
  * print_listint - print all the elements
  * @h: pointer
+ *
  * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t f;
+	size_t f = 1;
 
-	f = 0;
-	while (h != NULL)
+	if (h == NULL)
+		return (0);
+
+	while (h->next != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		f++;
 	}
+	printf("d\n", h-n);
 	return (f);
 }
