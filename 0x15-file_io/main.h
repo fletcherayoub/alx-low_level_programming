5bd9771b44561b438d0ffc3ba7452657001169d4
fletcherayoub <douch202@gmail.com>
2023-05-10 04:19:21 -0700

    update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,10 +5,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
-#include <stddef.h>
 #include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <string.h>
 
 int _putchar(char c);
